8ac762497af19d0810248ab5a1c840d3083e75de
Gigi M <mmgamao1@gmail.com>
2019-02-15 22:52:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,9 @@
-
-
 def oxford_comma(array)
+
   if array.length == 2
-    array.split(",")
-   if array.length > 2
-     array.split(",")
-     array[-1] << "and"
-    
-    
-end
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
